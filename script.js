a5178933bf099e34200ee7313c4e5dff1218594b
Seth Mulia <smulia@r2hstudent.com>
2022-01-17 22:35:38 -0500

    javascript update

---
+++ script.js
@@ -0,0 +1,16 @@
+///EASY///
+function min( firstNum, secondNum ) {
+	if ( firstNum < secondNum )
+      return firstNum;
+    else
+      return secondNum;
+}
+
+console.log(min(0, 10));
+// → 0
+console.log(min(0, -10));
+// → -10
+///EASY///
+
+
+////VERY EASY/////
