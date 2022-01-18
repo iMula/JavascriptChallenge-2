ac71ef5b28045c3eb85a96178096ce9fd6d649b7
Seth Mulia <smulia@r2hstudent.com>
2022-01-17 23:01:53 -0500

    java updates

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-///EASY///
+//VERY EASY: Write a function named min that takes two arguments and returns their minimum.//
+///VERY EASY///
 function min( firstNum, secondNum ) {
 	if ( firstNum < secondNum )
       return firstNum;
@@ -10,7 +11,17 @@ console.log(min(0, 10));
 // → 0
 console.log(min(0, -10));
 // → -10
-///EASY///
+///VERY EASY///
 
 
-////VERY EASY/////
+
+//EASY: Create an array of students holding their last name, first name, and age with 3 students. To validate, please log a greeting with the first name, last name and age of the 2nd student. The output should look like "Hello, my name is John Doe and I'm 19 years old."//
+
+////EASY/////
+let lastName  = ["Mulia", "James", "Davis"];
+let firstName = ["Seth", "Lebron", "Anthony"];
+let age = [21, 37, 28]
+
+let student = [lastName, firstName, age];
+
+console.log('Hello, my name is'(student));
